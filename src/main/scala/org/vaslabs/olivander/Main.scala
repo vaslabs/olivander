6da26f9f9015d1291bdc2288669628854e53b0c3
angelos georgiadis <angelos.georgiadis@autotrader.co.uk>
2017-10-29 03:28:36 +0000

    add http graphql schema

--- src/main/scala/org/vaslabs/olivander/Main.scala
+++ src/main/scala/org/vaslabs/olivander/Main.scala
@@ -10,23 +10,20 @@ import org.vaslabs.olivander.http.HttpRouter
 
 import scala.io.StdIn
 
-object Main {
+object Main extends App {
 
+  implicit val system = ActorSystem("OlivanderSystem")
+  implicit val materializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system.dispatcher
 
-  def main(args: Array[String]) {
-    implicit val system = ActorSystem("OlivanderSystem")
-    implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext = system.dispatcher
+  val httpRouter = new HttpRouter {}
+  val bindingFuture = Http().bindAndHandle(httpRouter.route, "localhost", 8080)
 
-    val httpRouter = new HttpRouter {}
-    val bindingFuture = Http().bindAndHandle(httpRouter.route, "localhost", 8080)
-
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
-  }
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
 
 }
