b1ca7942647f16ceeef5e1f1e9f10ffad47eec17
Vasilis Nicolaou <vaslabs@yahoo.gr>
2017-10-29 07:16:02 +0000

    Fixed when user is non-existent

--- src/main/scala/org/vaslabs/olivander/OrderHistory.scala
+++ src/main/scala/org/vaslabs/olivander/OrderHistory.scala
@@ -1,9 +1,9 @@
 package org.vaslabs.olivander
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props, Stash}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import org.vaslabs.olivander.domain.model.Order
 
-class OrderHistory private() extends Actor with ActorLogging with Stash{
+class OrderHistory private() extends Actor with ActorLogging {
 
   def receiveMoreItems(orders: List[Order]): Receive = {
     case o: Order =>
@@ -16,8 +16,7 @@ class OrderHistory private() extends Actor with ActorLogging with Stash{
     case o: Order =>
       log.info("Stored order {}", o)
       context.become(receiveMoreItems(List(o)))
-      unstashAll()
-    case g: OrderHistory.Get => stash()
+    case g: OrderHistory.Get => g.replyTo ! List.empty[Order]
   }
 
 }
